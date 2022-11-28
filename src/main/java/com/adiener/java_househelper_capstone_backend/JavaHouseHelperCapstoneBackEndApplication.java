95623b5ebf7ae848bbd71f5c881949ebf268cb43
Pochyxi <adienerlopez@gmail.com>
2022-11-28 10:25:57 +0100

    Initial commit

---
+++ src/main/java/com/adiener/java_househelper_capstone_backend/JavaHouseHelperCapstoneBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.adiener.java_househelper_capstone_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHouseHelperCapstoneBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaHouseHelperCapstoneBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adiener/java_househelper_capstone_backend/JavaHouseHelperCapstoneBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adiener.java_househelper_capstone_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaHouseHelperCapstoneBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
